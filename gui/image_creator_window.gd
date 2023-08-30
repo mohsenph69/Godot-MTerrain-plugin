4b653d2b9854a371bb3b0c545332e23330d68831
mohsenph69 <mohsen.ph.69@gmail.com>
2023-08-30 22:20:17 +0330

    Adding def color to image creator

--- gui/image_creator_window.gd
+++ gui/image_creator_window.gd
@@ -5,6 +5,7 @@ extends Window
 @onready var uniform_name_line := $VBoxContainer/uniform_name/uniform_name_line
 @onready var format_option := $VBoxContainer/uniform_name2/OptionButton
 @onready var uniform_name_empty_error:=$VBoxContainer/no_terrain2
+@onready var def_color_picker:= $VBoxContainer/def_color/ColorPickerButton
 
 var region_grid_size:Vector2i
 var region_pixel_size:int=0
@@ -27,6 +28,7 @@ func _on_close_requested():
 func _on_create_button_up():
 	var format:int = format_option.get_selected_id()
 	var uniform_name:String = uniform_name_line.text
+	var def_color:Color=def_color_picker.color
 	if uniform_name.is_empty():
 		printerr("Uniform Name is empty")
 		uniform_name_empty_error.visible = true
@@ -36,6 +38,7 @@ func _on_create_button_up():
 	for y in range(0,region_grid_size.y):
 		for x in range(0,region_grid_size.x):
 			var img:Image = Image.create(region_pixel_size,region_pixel_size,false,format)
+			img.fill(def_color)
 			var file_name = uniform_name + "_x"+str(x)+"_y"+str(y)+".res"
 			var file_path = data_dir.path_join(file_name)
 			ResourceSaver.save(img,file_path)
