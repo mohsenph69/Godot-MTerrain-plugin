3e6d8162aab7b7877edbd1f8770ab6bea958042d
bardia <mohsen.ph.69@gmail.com>
2024-05-18 08:19:25 +0330

    add octtree icon

--- gdextension/build_windows.sh
+++ gdextension/build_windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$SHELL -c "env -i scons platform=windows target=template_release arch=x86_32 $1"
-$SHELL -c "env -i scons platform=windows target=template_debug arch=x86_32 $1"
-$SHELL -c "env -i scons platform=windows target=template_release arch=x86_64 $1"
-$SHELL -c "env -i scons platform=windows target=template_debug arch=x86_64 $1"
+$SHELL -c "env -i scons platform=windows target=template_release arch=x86_32 generate_bindings=yes $1"
+$SHELL -c "env -i scons platform=windows target=template_debug arch=x86_32 generate_bindings=yes $1"
+$SHELL -c "env -i scons platform=windows target=template_release arch=x86_64 generate_bindings=yes $1"
+$SHELL -c "env -i scons platform=windows target=template_debug arch=x86_64 generate_bindings=yes $1"
