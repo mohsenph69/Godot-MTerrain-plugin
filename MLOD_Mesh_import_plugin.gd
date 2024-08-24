f504d44e5a3cf805f6825dc9290d0dcfecbac76b
rossunger <ross93@gmail.com>
2024-08-24 02:45:31 -0500

    mmesh_lod import bugfix

--- MLOD_Mesh_import_plugin.gd
+++ MLOD_Mesh_import_plugin.gd
@@ -35,10 +35,11 @@ func _import(source_file, save_path, options, platform_variants, gen_files):
 	var gltf_document_load = GLTFDocument.new()
 	var gltf_state_load = GLTFState.new()
 	var error = gltf_document_load.append_from_file(source_file, gltf_state_load)
-	var mmesh_lod = MMeshLod.new()
+	var mmesh_lod = MMeshLod.new()	
 	if error == OK:		
 		var gltf_scene_root_node = gltf_document_load.generate_scene(gltf_state_load)
-		for mesh in gltf_state_load.meshes:
-			mmesh_lod.meshes.push_back(	mesh.mesh.get_mesh() )
-	var filename = save_path + ".res"
+		mmesh_lod.meshes.resize(gltf_state_load.meshes.size())
+		for i in gltf_state_load.meshes.size():
+			mmesh_lod.meshes[i] =  gltf_state_load.meshes[i].mesh.get_mesh() 
+	var filename = save_path + ".glb"
 	return ResourceSaver.save(mmesh_lod, filename)	
