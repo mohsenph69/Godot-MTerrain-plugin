18af9c501032af291e8baf45a1e1881548c8e5bf
Mohsen Zare <mohsen.ph.69@gmail.com>
2025-03-03 23:39:39 +0330

    add gizmo to MObstacle and fix some nav bugs

--- gdextension/src/navmesh/mobstacle.cpp
+++ gdextension/src/navmesh/mobstacle.cpp
@@ -11,6 +11,9 @@ void MObstacle::_bind_methods(){
     ClassDB::bind_method(D_METHOD("get_depth"), &MObstacle::get_depth);
     ClassDB::bind_method(D_METHOD("set_depth","input"), &MObstacle::set_depth);
     ADD_PROPERTY(PropertyInfo(Variant::FLOAT,"depth"),"set_depth","get_depth");
+
+    ClassDB::bind_method(D_METHOD("has_gizmo"), &MObstacle::has_gizmo);
+    ClassDB::bind_method(D_METHOD("get_aabb"), &MObstacle::get_aabb);
 }
 
 
@@ -27,10 +30,20 @@ float MObstacle::get_width(){
 }
 void MObstacle::set_width(float input){
     width = input;
+    update_gizmos();
 }
 float MObstacle::get_depth(){
     return depth;
 }
 void MObstacle::set_depth(float input){
     depth = input;
+    update_gizmos();
+}
+
+bool MObstacle::has_gizmo() const{
+    return true;
+}
+
+AABB MObstacle::get_aabb() const{
+    return AABB(Vector3(-width/2.0,-10.0,-depth/2.0),Vector3(width,20.0,depth));
 }
